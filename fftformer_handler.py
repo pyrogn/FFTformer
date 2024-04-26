c6c329f7a03b0f212797df045080cb3b0832c896
Pavel <60060559+pyrogn@users.noreply.github.com>
2024-04-26 16:31:35 +0300

    Leave only one class in handler

--- fftformer_handler.py
+++ fftformer_handler.py
@@ -93,56 +93,38 @@ class FFTformerHandler(BaseHandler):
         return images
 
 
-class ModelHandler(object):
-    """
-    A custom model handler implementation.
-    """
-
-    def __init__(self):
-        self._context = None
-        self.initialized = False
-        self.model = None
-        self.device = None
-
-    def initialize(self, context):
-        """
-        Invoke by torchserve for loading a model
-        :param context: context contains model server system properties
-        :return:
-        """
-
-        #  load the model
-        self.manifest = context.manifest
-
-        properties = context.system_properties
-        model_dir = properties.get("model_dir")
-        self.device = torch.device(
-            "cuda:" + str(properties.get("gpu_id"))
-            if torch.cuda.is_available()
-            else "cpu"
-        )
-
-        # Read model serialize/pt file
-        serialized_file = self.manifest["model"]["serializedFile"]
-        model_pt_path = os.path.join(model_dir, serialized_file)
-        if not os.path.isfile(model_pt_path):
-            raise RuntimeError("Missing the model.pt file")
-
-        self.model = torch.jit.load(model_pt_path)
-
-        self.initialized = True
-
-    def handle(self, data, context):
-        """
-        Invoke by TorchServe for prediction request.
-        Do pre-processing of data, prediction using model and postprocessing of prediciton output
-        :param data: Input data for prediction
-        :param context: Initial context contains model server system properties.
-        :return: prediction output
-        """
-        pred_out = self.model.forward(data)
-        return pred_out
+# model = fftformer()
 
 
-# The entry point for the script is the handler name
-_service = FFTformerHandler()
+# def entry_point_function_name(data, context):
+#     """
+#     Works on data and context to create model object or process inference request.
+#     Following sample demonstrates how model object can be initialized for jit mode.
+#     Similarly you can do it for eager mode models.
+#     :param data: Input data for prediction
+#     :param context: context contains model server system properties
+#     :return: prediction output
+#     """
+#     global model
+
+#     if not data:
+#         manifest = context.manifest
+
+#         properties = context.system_properties
+#         model_dir = properties.get("model_dir")
+#         device = torch.device(
+#             "cuda:" + str(properties.get("gpu_id"))
+#             if torch.cuda.is_available()
+#             else "cpu"
+#         )
+
+#         # Read model serialize/pt file
+#         serialized_file = manifest["model"]["serializedFile"]
+#         model_pt_path = os.path.join(model_dir, serialized_file)
+#         if not os.path.isfile(model_pt_path):
+#             raise RuntimeError("Missing the model.pt file")
+
+#         model = torch.jit.load(model_pt_path)
+#     else:
+#         # infer and return result
+#         return model(data)
